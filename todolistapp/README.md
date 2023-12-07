b8559ed01e67a5a3e1fdaea5c9021e6d218e238a
gangisettisumanth <112507977+gangisettisumanth@users.noreply.github.com>
2023-12-07 17:18:46 +0530

    Add files via upload

---
+++ todolistapp/README.md
@@ -0,0 +1,45 @@
+#TodoList Application 
+
+Overview
+Build a Todo application using React.js that allows users to perform CRUD (Create, Read, Update, Delete) operations on tasks. The application fetches initial data from a remote API using Axios.
+
+Features:-
+
+#Display Existing Tasks:
+Show a list of existing tasks, including the task name and its completion status (completed or not).
+Initialize this list with todos fetched from the API.
+
+
+#API Endpoint:
+Utilize the following API endpoint to retrieve todos: https://jsonplaceholder.typicode.com/users/1/todos.
+
+#Task Properties:
+Each todo object from the API response has the following properties:
+id (number): The unique identifier for the todo.
+title (string): The title of the todo.
+completed (boolean): Indicates whether the todo has been completed or not.
+
+#Add New Tasks:
+Provide an input field to add new tasks.
+Validate before adding a task:
+The task name should not be empty.
+
+#Mark Tasks as Completed:
+Allow users to mark a task as completed by clicking on it.
+Visually distinguish completed tasks (e.g., by using a different color).
+
+#Edit Task Name
+Enable users to edit the task name by clicking on an edit button next to each task.
+When a task is edited, update the task name in the list.
+
+#Delete Tasks
+Provide a delete button to allow users to remove a task from the list.
+Filter Tasks
+
+npm start
+Open the application in your browser at http://localhost:3000.
+
+Technologies Used
+React.js
+Axios
+CSS (for styling)
---
+++ todolistapp/src/App.js
@@ -0,0 +1,149 @@
+
+import React, { useState, useEffect, useReducer } from 'react';
+import axios from 'axios';
+import './App.css';
+
+const TodoList = ({ todos, dispatch, setEditTodoText }) => {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          <span
+            style={{
+              textDecoration: todo.completed ? 'line-through' : 'none',
+              color: todo.completed ? 'red' : 'black',
+              cursor: 'pointer',
+              textAlign: 'left',
+            }}
+            onClick={() => dispatch({ type: 'TOGGLE_TODO', payload: todo.id })}
+          >
+            {todo.title}
+          </span>
+          <button onClick={() => setEditTodoText(todo.id)} style={{ width: '90px', marginLeft: '10px' }}>
+            Edit
+          </button>
+          <button onClick={() => dispatch({ type: 'DELETE_TODO', payload: todo.id })} style={{ width: '90px', marginLeft: '10px' }}>
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const initialState = {
+  todos: [],
+  editedTodo: null,
+};
+
+function todoReducer(state, action) {
+  switch (action.type) {
+    case 'FETCH_TODOS':
+      return { todos: action.payload, editedTodo: null };
+    case 'ADD_TODO':
+      return { todos: [...state.todos, action.payload], editedTodo: null };
+    case 'TOGGLE_TODO':
+      return {
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload ? { ...todo, completed: !todo.completed } : todo
+        ),
+        editedTodo: null,
+      };
+    case 'DELETE_TODO':
+      return { todos: state.todos.filter((todo) => todo.id !== action.payload), editedTodo: null };
+    case 'EDIT_TODO':
+      return { todos: state.todos, editedTodo: action.payload };
+    case 'UPDATE_TODO':
+      return {
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload.id ? { ...todo, title: action.payload.title } : todo
+        ),
+        editedTodo: null,
+      };
+    default:
+      return state;
+  }
+}
+
+const App = () => {
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+  const [newTodo, setNewTodo] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users/1/todos')
+      .then((response) => dispatch({ type: 'FETCH_TODOS', payload: response.data }))
+      .catch((error) => console.error('Error fetching todos:', error));
+  }, []);
+
+  const addTodo = () => {
+    if (newTodo.trim() === '') {
+      alert('Please enter the text before adding a task.');
+      return;
+    }
+
+    const newTask = {
+      id: Date.now(),
+      title: newTodo,
+      completed: false,
+    };
+
+    dispatch({ type: 'ADD_TODO', payload: newTask });
+    setNewTodo('');
+  };
+
+  const updateTodo = () => {
+    if (newTodo.trim() === '') {
+      alert('Please enter the text before updating the task.');
+      return;
+    }
+
+    dispatch({ type: 'UPDATE_TODO', payload: { id: state.editedTodo, title: newTodo } });
+    setNewTodo('');
+  };
+
+  const setEditTodoText = (todoId) => {
+    const todoToEdit = state.todos.find((todo) => todo.id === todoId);
+    if (todoToEdit) {
+      setNewTodo(todoToEdit.title);
+      dispatch({ type: 'EDIT_TODO', payload: todoId });
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>TodoList App</h1>
+      <div className="box">
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Add a new task"
+          style={{
+            padding: '12px',
+            fontSize: '16px',
+            border: '1px solid #ccc',
+            borderRadius: '5px',
+            margin: '10px',
+          }}
+        />
+        <button onClick={state.editedTodo !== null ? updateTodo : addTodo}>
+          {state.editedTodo !== null ? 'Update Task' : 'Add Task'}
+        </button>
+      </div>
+
+      <TodoList todos={state.todos} dispatch={dispatch} setEditTodoText={setEditTodoText} />
+    </div>
+  );
+};
+
+export default App;
+
+
+
+
+
+
+
+
+
